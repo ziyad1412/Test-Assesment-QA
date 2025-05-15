31344fae5e1f9fd47d9e7538b73c578d2d42b057
ziyad1412 <abdurrahman.ziyad17@gmail.com>
2025-05-15 22:52:40 +0700

    Fix

--- cypress/e2e/paragon/login.cy.js
+++ cypress/e2e/paragon/login.cy.js
@@ -2,12 +2,20 @@
 describe("Test Automation Register", () => {
   // Test Login
   it("Submit data via login", () => {
-    //kunjungi website
+    // 1. Kunjungi halaman login
     cy.visit("https://recruitment-staging-queenbee.paradev.io/login");
+
+    // 2. Klik ikon untuk menutup popup atau modal jika ada
     cy.get('svg[style="cursor: pointer;"]').click({ force: true });
+
+    // 3. Pilih tab login dengan email
     cy.get("#page-login__tabs-email").click();
+
+    // 4. Masukkan email dan password untuk login
     cy.get('[name="email"]').type("ziyad@gmail.com");
     cy.get('[name="password"]').type("Ziyad.123");
+
+    // 5. Klik tombol login
     cy.get("#page-login__button-login").click();
   });
 });
